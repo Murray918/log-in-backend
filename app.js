7eaa4ffd77ceaf0949867264b38c4d15c49661aa
Andrew Murray <darbyguitars@gmail.com>
2017-08-17 15:51:01 -0500

    secind

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('blam');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.get('/', function(req, res) {
+
+ res.render('home')
+
+})
+
+app.listen(3000, function() {
+  console.log('Successfully started express application!'
+);
+})
